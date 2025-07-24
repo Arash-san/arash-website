e736d28f5cd88b000f96a7441fcc9bb3346b418e
Arash <arash.ahmadi-1@ou.edu>
2025-07-24 04:41:47 -0500

    Fixing some small bugs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/arash-website",
+  assetPrefix: "/arash-website",
+  output: "export",
 };
 
 export default nextConfig;
